7e3aba49432472b6c9e3ced0f51e2bc7665769fd
Masayoshi Asakura <asache3@gmail.com>
2014-06-19 01:42:52 +0900

    Modify method practice

--- 04_method.rb
+++ 04_method.rb
@@ -14,3 +14,23 @@ end
 
 hello()
 hello("Newbie")
+
+
+# 可変長引数
+def sum(*numbers)
+  sum = 0
+  numbers.each {|n| sum += n}
+  puts sum
+end
+
+my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum(*my_numbers)
+
+
+# ブロック付きメソッド
+# yieldのところで呼び出し元のブロックが実行される
+def number(n)
+  yield n
+end
+
+number(2){|x| puts x * 3}
